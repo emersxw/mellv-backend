855e5528fcc3869b31fead7779eace80ae1282ee
Emerson Lopes <lupuselit@outlook.com>
2019-09-06 12:09:40 -0300

    -f

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,15 +4,15 @@ import database from '../database';
 const router = express.Router();
 
 router.get('/', async (request, response) => {
-  const { rows } = await database.query('SELECT * FROM books');
+  const { rows } = await database.query('SELECT * FROM users');
 
   response.status(200).json(rows);
 });
 
 router.post('/', async (request, response) => {
-  const { author, title } = request.body
+  const { name, email } = request.body
 
-  await database.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title]);
+  await database.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
 
   response.status(201).json({status: 'success', message: 'book created'})
 });
--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(cors());
 
-import bookRoutes from './controllers/bookController';
+import userRoutes from './controllers/userController';
 
-app.use('/api', bookRoutes);
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('magic happens on port ' + port));
