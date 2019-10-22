b8c09dd1dc6f4e28d156c3ca38eff70d89707e01
Emerson Lopes <lupuselit@outlook.com>
2019-10-22 17:46:19 -0300

    fix get product route

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -103,8 +103,9 @@ router.put('/:id', async (request, response) => {
 
 router.get('/:id', async (request, response) => {
   const { id } = request.params;
-  const product = await database.query('SELECT * FROM products WHERE id = $1', [id]);
-  return response.status(200).send({ product });
+  const { rows } = await database.query('SELECT * FROM products WHERE id = $1', [id]);
+  return response.status(200).json({ product: rows[0] });
+
 });
 
 router.delete('/:id', async (request, response) => {
